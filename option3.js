1e7d0968b8ba3d3c0aa35cefa488dcc50937bf33
cloprothro@gmail.com <clostar.delta@gmail.com>
2024-02-29 14:08:49 -0800

    html and css added w/ js updated

--- option3.js
+++ option3.js
@@ -30,17 +30,17 @@ function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
     while (currentIndex > 0) {
   
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
   
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
   
     return array;
-  }
+}
 
 shuffle(potentialDates);
 
