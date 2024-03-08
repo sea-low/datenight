0d90bee22adf3e1f90956dc2be34ad06955967ae
cloprothro@gmail.com <clostar.delta@gmail.com>
2024-03-08 12:38:29 -0800

    update

--- option3.js
+++ option3.js
@@ -1,27 +1,28 @@
 const potentialDates = [
-    {dateType: 'in', dateActivity: 'video games'},
-    {dateType: 'in', dateActivity: 'board games'},
-    {dateType: 'in', dateActivity: 'movie night'},
-    {dateType: 'in', dateActivity: 'hobby/project night'},
-    {dateType: 'in', dateActivity: 'lego'},
-    {dateType: 'in', dateActivity: 'cat night'},
-    {dateType: 'in', dateActivity: 'yoga'},
-    {dateType: 'in', dateActivity: 'painting night'},
-    {dateType: 'in', dateActivity: 'try brand new recipe'},
-    {dateType: 'in', dateActivity: 'competitive origami'},
-    {dateType: 'out', dateActivity: 'fishing'},
-    {dateType: 'out', dateActivity: 'mini golf'},
-    {dateType: 'out', dateActivity: 'picnic and walk'},
-    {dateType: 'out', dateActivity: 'double date with friends'},
-    {dateType: 'out', dateActivity: 'try a new food place'},
-    {dateType: 'out', dateActivity: 'first date date'},
-    {dateType: 'out', dateActivity: 'museum'},
-    {dateType: 'out', dateActivity: 'hike'},
-    {dateType: 'out', dateActivity: 'vr laser tag'},
-    {dateType: 'out', dateActivity: 'water gun fight at piedra'},
-    {dateType: 'out', dateActivity: 'bumper cart'},
-    {dateType: 'out', dateActivity: 'day trip'},
-    {dateType: 'out', dateActivity: 'shooting range'},
+    {dateType: 'in', dateActivity: 'Gaming Night: Video Game Edition', dateDesciption: 'Choose a video game to play together! Hop on a discord call and enjoy games and conversation.'},
+    {dateType: 'in', dateActivity: 'Gaming Night: Board Game Edition', dateDesciption: 'Select a two player board game and get your competitive side on!'},
+    {dateType: 'in', dateActivity: 'Movie Night', dateDesciption: 'Make some snacky food for dinner and get cozy for a movie night!'},
+    {dateType: 'in', dateActivity: 'Hobby/Project Night', dateDesciption: 'Pick a project that one of you is working on and help the other one out working on it!'},
+    {dateType: 'in', dateActivity: 'Lego', dateDesciption: 'Grab a set of legos--either a large set that can be done together or individual ones--and enjoy.'},
+    {dateType: 'in', dateActivity: 'Cat Night', dateDesciption: 'Follow around the cat and act like her for a few hours.'},
+    {dateType: 'in', dateActivity: 'Yoga', dateDesciption: 'Find a yoga video on Youtube and follow along.'},
+    {dateType: 'in', dateActivity: 'Painting Night', dateDesciption: 'Select a guided painting video and paint along with it. Best paired with charcuterie.'},
+    {dateType: 'in', dateActivity: 'Try Brand New Recipe', dateDesciption: 'All those TikToks and Instagram recipes that you have shared to each other and drooled over? Tonight, y`all are making one!'},
+    {dateType: 'in', dateActivity: 'Competitive Origami', dateDesciption: 'Find a Youtube video for an origami that neither of you have made. See who can fold it the best!'},
+    {dateType: 'in', dateActivity: 'Fort Night', dateDesciption: 'Find as many blankets as possible and work together to build a fort! What do you do in the fort? Well...it`s up to your imagination.'},
+    {dateType: 'out', dateActivity: 'Gone Fishing', dateDesciption: 'Two poles. The water. The serentiy. What more do you need?'},
+    {dateType: 'out', dateActivity: 'Mini Golf', dateDesciption: 'Time to get your golf on! Who knows? Maybe one of you will win that Icee.'},
+    {dateType: 'out', dateActivity: 'Picnic and Walk', dateDesciption: 'All you need is some sandwiches or '},
+    {dateType: 'out', dateActivity: 'Double Date with Friends', dateDesciption: 'Hit up the buds and see who wants to do an activity out!'},
+    {dateType: 'out', dateActivity: 'Try a New Food Place', dateDesciption: 'Remember that place you drove by and thought `I wonder what that place is like?` Today, you`re trying it!'},
+    {dateType: 'out', dateActivity: '"First Date" Date', dateDesciption: 'Go thrifting. Pick outfits for each other. Make up a character. Select a place that you don`t normally go to and have a first date!'},
+    {dateType: 'out', dateActivity: 'Museum', dateDesciption: 'Find a museum or historical site and check it out together.'},
+    {dateType: 'out', dateActivity: 'Hike', dateDesciption: 'Get your comfy clothes and good walking shoes on and hit the trail!'},
+    {dateType: 'out', dateActivity: 'VR Laser Tag', dateDesciption: 'Head on over to Broken Controller and see who can '},
+    {dateType: 'out', dateActivity: 'Water Gun Fight at Piedra', dateDesciption: 'Load up your super soakers and get ready for the hunt!'},
+    {dateType: 'out', dateActivity: 'Bumper Cars', dateDesciption: 'Go to the track and hit on each other!'},
+    {dateType: 'out', dateActivity: 'Day Trip', dateDesciption: 'Plan out a day trip together and hit the road.'},
+    {dateType: 'out', dateActivity: 'Shooting Range', dateDesciption: 'Go to the shooting range and see who can get the most bullseyes!'},
 ];
 
 let selectedDates = [];
@@ -42,8 +43,6 @@ function shuffle(array) {
     return array;
 }
 
-shuffle(potentialDates);
-
 const inFilter = (element) => {
 	return element.dateType === "in";
 };
@@ -55,3 +54,59 @@ const outFilter = (element) => {
 const outs = potentialDates.filter(outFilter);
 
 const ins = potentialDates.filter(inFilter);
+
+const dateDescription = [];
+
+function checkButton() {
+    if(document.getElementById("in").checked === true) {
+        shuffle(ins);
+        selectedDates.push(ins[0].dateActivity);
+        dateDescription.push(ins[0].dateDesciption);
+       // DateDescription.push(ins[0])
+    } else if(document.getElementById("out").checked === true) {
+        shuffle(outs);
+        selectedDates.push(outs[0].dateActivity);
+        dateDescription.push(outs[0].dateDesciption);
+    } else {
+        shuffle(potentialDates);
+        selectedDates.push(potentialDates[0].dateActivity);
+        dateDescription.push(potentialDates[0].dateDesciption);
+    };
+    document.getElementById("date-activity").innerHTML=`${selectedDates[(selectedDates.length - 1)]}`
+    document.getElementById("date-description").innerHTML=`${dateDescription[(dateDescription.length - 1)]}`
+    console.log(selectedDates);
+}
+
+function selectOnePlease() {
+    const form = document.querySelector('#createNewDate');
+    const checkboxes = form.querySelectorAll('input[type=checkbox]');
+    const checkboxLength = checkboxes.length;
+    const firstCheckbox = checkboxLength > 0 ? checkboxes[0] : null;
+
+    function init() {
+        if (firstCheckbox) {
+            for (let i = 0; i < checkboxLength; i++) {
+                checkboxes[i].addEventListener('change', checkValidity);
+            }
+
+            checkValidity();
+        }
+    }
+
+    function isChecked() {
+        for (let i = 0; i < checkboxLength; i++) {
+            if (checkboxes[i].checked) return true;
+        }
+
+        return false;
+    }
+
+    function checkValidity() {
+        const errorMessage = !isChecked() ? 'At least one checkbox must be selected.' : '';
+        firstCheckbox.setCustomValidity(errorMessage);
+    }
+
+    init();
+};
+
+$('div.checkbox-group.required :checkbox:checked').length > 0
